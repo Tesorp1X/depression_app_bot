8aa6e4697cf54a4d207b97dc782977a2dfaa3b74
Tesorp1X <rakteam2014@gmail.com>
2021-07-09 07:19:38 +0700

    Implemented /more{id} command handler.

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,8 +1,10 @@
-from aiogram.types import Message
+from aiogram.types import Message, ParseMode
+from aiogram.dispatcher import FSMContext
+
 from bot.config import dp
 from bot.states import NewNoteStates
 from bot.services.dbService.account_service import register_new_user, is_user_exist
-from bot.services.dbService.note_service import get_notes_for_user
+from bot.services.dbService.note_service import get_notes_for_user, get_note_by_id
 
 
 @dp.message_handler(commands="start", state="*")
@@ -25,14 +27,46 @@ async def help_message_handler(message: Message):
 
 
 @dp.message_handler(commands="list", state="*")
-async def list_message_handler(message: Message):
+async def list_message_handler(message: Message, state: FSMContext):
     # TODO: ALTER THE MESSAGE TEXT
     # get list from db and print
 
     msg = "list of your spendings for the last month...\n\n"
-    template = "{id} {name} -- {val}\n"
-    for note in get_notes_for_user(t_id=message.from_user.id):
+    template = "{name} -- {val} /more{id} /del{id}\n"
+    last_notes = get_notes_for_user(t_id=message.from_user.id)
+    await state.update_data(last_notes=last_notes)
+    for note in last_notes:
         msg += template.format(id=note.id, name=note.name, val=note.value)
     await message.answer(msg)
 
 # TODO: MAKE A /menu COMMAND WITH EVERY FUNCTION AS CALLBACK QUERY BUTTONS
+
+
+@dp.message_handler(lambda message: message.text.startswith('/more'))
+async def more_message_handler(message: Message, state: FSMContext):
+    note_id = int(message.text[5:])
+
+    data = await state.get_data()
+
+    if 'last_notes' in data.keys():
+        last_notes = data.get('last_notes')
+        for note in last_notes:
+            if note.id == note_id:
+                that_note = note
+    else:
+        # last_notes = get_notes_for_user(t_id=message.from_user.id)
+        # data['last_notes'] = last_notes
+        that_note = get_note_by_id(note_id)
+
+    description = "*Note description:*" + that_note.description \
+        if that_note.description else "*This not doesn't have a description.*"
+    msg = f"*#{that_note.id} {that_note.name}@{that_note.created_at}*\n\n{description}"
+
+    await message.answer(msg, parse_mode=ParseMode.MARKDOWN)
+
+
+@dp.message_handler(lambda message: message.text.startswith('/del'))
+async def more_message_handler(message: Message, state: FSMContext):
+    note_id = int(message.text[4:])
+
+    await message.answer("this function doesn't implemented yet.")
--- bot/services/dbService/note_service.py
+++ bot/services/dbService/note_service.py
@@ -1,5 +1,7 @@
 from sqlite3 import InternalError
 
+from typing import Optional, List
+
 from .models.note_model import NoteModel
 from .models.user_model import UserModel
 
@@ -28,8 +30,12 @@ def create_new_note(name: str, value: int, author_t_id: int,
     return note.get_id()
 
 
-def get_notes_for_user(t_id: int, amount=3):
+def get_notes_for_user(t_id: int, amount=3) -> Optional[List[NoteModel]]:
     # TODO: check date
     query = (NoteModel.select().join(UserModel).where(UserModel.t_id == t_id))
 
     return [note for note in query]
+
+
+def get_note_by_id(note_id: int) -> Optional[NoteModel]:
+    return NoteModel.get_or_none(id=note_id)
